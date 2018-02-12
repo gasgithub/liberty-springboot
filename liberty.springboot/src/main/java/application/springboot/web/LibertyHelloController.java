39b4968a43abc3f5842c827561924a79637b2159
gas <gas@9.167.233.177>
2018-02-12 15:24:40 +0100

    testy parametrow

--- liberty.springboot/src/main/java/application/springboot/web/LibertyHelloController.java
+++ liberty.springboot/src/main/java/application/springboot/web/LibertyHelloController.java
@@ -8,6 +8,10 @@ public class LibertyHelloController {
 
     @RequestMapping("/springbootweb")
     public String hello() {
+    	System.out.println(System.getProperty("default.http.port"));
+    	System.out.println(System.getProperty("bulba"));
+    	System.out.println(System.getProperty("bulbaBP"));
+    	System.out.println(System.getenv("my_env"));
         return "Hello from Spring Boot MVC running on Liberty!";
     }
 
